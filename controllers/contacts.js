bbdc6d728489313a92841aa9a9c3ea44aaee7885
ekpenyongeetim <ekpenyongeetim@gmail.com>
2024-01-17 13:20:02 +0100

    mispelling corrected in getSingle()

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-  const contactId = new ObjectId(req.param.id);
+  const contactId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDatabase()
     .db()
